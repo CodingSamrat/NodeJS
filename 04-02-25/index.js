279a3e2eb72d9052ad28362ee2580f6970b00461
Sam <samratbiswas986@gmail.com>
2025-02-04 19:01:08 +0530

    04-02-25

---
+++ 04-02-25/index.js
@@ -0,0 +1,91 @@
+
+
+const users1 = ['Santo', 'Bipro']
+const users2 = ['Badal', 'Sam', 'Rahul']
+
+const users = [...users1, ...users2]
+
+
+// users.push('Badol')
+// users.push('Badol')
+
+
+// add new element at last position of array
+// users.push('Dom')
+
+
+console.log(users)
+
+
+// remove the last element
+// users.pop()
+// users.pop()
+
+
+// Access value at index
+// console.log(users.at(3))
+// console.log(users[3])
+
+// console.log(users.indexOf('Rahul'))
+// console.log(users.indexOf('Badal'))
+
+// Convert the array to a comma separated string
+// console.log(users.toString())
+
+
+
+// console.log('Santo_Bipro_Badal'.split('_'))
+// 'Santo_Bipro_Badal_Sam_Rahul'
+
+// console.log(users.join('  '))
+
+
+
+
+
+// let i = 0
+
+// while (true) {
+//     console.log(i)
+
+// sa
+// }
+
+
+// console.log(users3[0])
+// console.log(users3[1])
+// console.log(users3[2])
+
+
+
+
+
+
+// console.log(`1. Name is - ${users3[0]}`)
+// console.log(`2. Name is - ${users3[1]}`)
+// console.log(`3. Name is - ${users3[2]}`)
+// console.log(`4. Name is - ${users3[3]}`)
+const users3 = ['Badal', 'Sam', 'Rahul', 'Santo', 'Bipro']
+
+
+//for (let i = 0; i < users3.length; i++) {
+
+// console.log(`${i + 1}. Name is - ${users3[i]}`)
+// }
+
+//1 - odd
+//2 - even
+
+// for (let i = 0; i <= 20; i++) {
+//     console.log(i);
+// }
+let number = 20;
+
+if (number % 2 === 0) {
+    console.log("even");
+
+
+
+} else {
+    console.log("odd");
+}
