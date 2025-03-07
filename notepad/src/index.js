243aed0afac85ed61738f4d1d4727cc33615c3a2
Sam <samratbiswas986@gmail.com>
2025-03-07 15:21:21 +0530

    object key value

--- notepad/src/index.js
+++ notepad/src/index.js
@@ -1,5 +1,5 @@
 // CommonJS
-// const { showNotes, showNoteById } = require("./note"); 
+// const { showNotes, showNoteById } = require("./note");
 
 // ESM
 // import { showNoteById, showNotes } from "./note.js"
@@ -8,10 +8,10 @@
 
 
 // showNotes()
-// showNoteById(1) 
+// showNoteById(1)
 
 
-import notes, { showNoteById, addNote } from "./note.js";
+// import notes, { showNoteById, addNote } from "./note.js";
 
 
 
@@ -19,22 +19,52 @@ import notes, { showNoteById, addNote } from "./note.js";
 
 // console.log(notes)
 
-const notesOne = []
-for (let i = 0; i < notes.length; i++) {
-    const note = notes[i];
+// const notesOne = []
+// for (let i = 0; i < notes.length; i++) {
+//     const note = notes[i];
 
-    notesOne.push(note.id == 2 ? note : null)
-
-
-    // if (note.id == 2) {
-    //     notesOne.push(note)
-    // } else {
-    //     notesOne.push(null)
-    // }
-
-}
-console.log(notesOne);
+//     notesOne.push(note.id == 2 ? note : null)
 
 
-const newNotes = notes.map((note) => { console.log(note) })
-console.log(newNotes)
+//     // if (note.id == 2) {
+//     //     notesOne.push(note)
+//     // } else {
+//     //     notesOne.push(null)
+//     // }
+
+// }
+// //console.log(notesOne);
+
+
+// const newNotes = notes.map((note) => { console.log(note) })
+// //console.log(newNotes)
+
+
+// import { Array_Object } from "./index1.js";
+
+// const result = Array_Object
+
+// for (let i = 0; i < result.length; i++) {
+//     const result = Object.keys(Array_Object[i]);
+//     console.log(result);
+// }
+
+
+
+const notes = [
+    { id: 1, title: "Meeting Notes", note: "Discuss project timeline." },
+    { id: 2, title: "Shopping List", note: "Milk, eggs, bread, butter." },
+    { id: 3, title: "Ideas", note: "Work on a new blog post." }
+];
+
+const keys = Object.keys(notes[1]);
+console.log(keys);
+
+const values = Object.values(notes[1]);
+console.log(values);
+
+
+// for (let i = 0; i < notes.length; i++) {
+//     const result = Object.keys(notes[1]);
+//     console.log(result);
+// }
---
+++ notepad/src/index1.js
@@ -0,0 +1,24 @@
+export const Array_Object = [
+
+    {
+        id: 1,
+        name: "Badol Majumdar",
+        mobile: 7001412706,
+    },
+    {
+        id: 2,
+        name: "Shomrat Biswas",
+        mobile: 3001412706,
+    },
+    {
+        id: 3,
+        name: "Rudar Barmon",
+        mobile: 2001412706,
+    },
+]
+
+
+// for (let i = 0; i < Array_Object.length; i++) {
+//     const result = Object.keys(Array_Object[i]);
+//     console.log(result);
+// }
